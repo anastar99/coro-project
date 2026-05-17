9c63841638cb16ea7b539865bdcf4e5126e0ced2
Brandon <119729981+1Leon8@users.noreply.github.com>
2026-05-16 19:57:40 -0700

    Add setlists

--- backend/internal/setlists/repository.go
+++ backend/internal/setlists/repository.go
@@ -1,6 +1,7 @@
 package setlists
 
 import (
+	"fmt"
 	"context"
 	"database/sql"
 )
@@ -13,8 +14,39 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db: db}
 }
 
-func (r *Repository) GetAllSetlists(context.Context) ([]SetList, error) {
+func (r *Repository) GetAllSetlists(ctx context.Context) ([]SetList, error) {
 	var setList []SetList
+
+	query := `SELECT setlist_id, setlist_name FROM setlists`
+
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		fmt.Println("failed to get setlists:", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var setlist SetList
+
+		err := rows.Scan(
+			&setlist.SetListID,
+			&setlist.SetListName,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		setList = append(setList, setlist)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return setList, nil
+}
+
 
 	// TODO: need to return an array of objects something like
 	// [
@@ -30,7 +62,3 @@ func (r *Repository) GetAllSetlists(context.Context) ([]SetList, error) {
 	// 	...
 	//
 	// ]
-
-	return setList, nil
-
-}
