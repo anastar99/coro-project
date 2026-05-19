e02e250af0c80934109d2b09ef0ef2da71faec5b
Brandon <119729981+1Leon8@users.noreply.github.com>
2026-05-19 15:59:04 -0700

    GetASong, GetAllSongs, DeleteSongs implemented

--- backend/internal/setlists/model.go
+++ backend/internal/setlists/model.go
@@ -1,6 +1,13 @@
 package setlists
 
+import "github.com/anastar99/coro-project/backend/internal/songs"
+
 type SetList struct {
-	SetListID   int    `json:"setlist_id"`
-	SetListName string `json:"setlist_name"`
+	SetListID    int          `json:"setlist_id"`
+	SetListName  string       `json:"setlist_name"`
+	SetListSongs []songs.Song `json:"songs"`
+}
+
+type CreateSetList struct {
+	SetListName  string       `json:"setlist_name"`
 }
