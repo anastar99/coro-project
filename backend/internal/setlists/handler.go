4fd3db9f6f534b1d7704a9c65e746f4289ad9518
Brandon <119729981+1Leon8@users.noreply.github.com>
2026-05-25 17:16:17 -0700

    Fixed hanlder error

--- backend/internal/setlists/handler.go
+++ backend/internal/setlists/handler.go
@@ -8,13 +8,11 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-// IMPORTS
-
 type Handler struct {
 	service *Service
 }
 
-func NewHanlder(service *Service) *Handler {
+func NewHandler(service *Service) *Handler {
 	return &Handler{service: service}
 }
 
@@ -22,10 +20,9 @@ func (h *Handler) GetAllSetlists(
 	w http.ResponseWriter,
 	r *http.Request,
 ) {
-
 	setlists, err := h.service.GetAllSetlists(r.Context())
 	if err != nil {
-		http.Error(w, "failed to get setlist", http.StatusInternalServerError)
+		http.Error(w, "failed to get setlists", http.StatusInternalServerError)
 		return
 	}
 
@@ -38,14 +35,37 @@ func (h *Handler) GetSetlist(
 	w http.ResponseWriter,
 	r *http.Request,
 ) {
-	setlist_id, err := strconv.Atoi(chi.URLParam(r, "id"))
-
+	setlistID, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		http.Error(w, "invalid setlist id", http.StatusBadRequest)
 		return
 	}
 
-	setlist, err :=h.service.GetSetlist(r.Context(), setlist_id)
+	setlist, err := h.service.GetSetlist(r.Context(), setlistID)
+	if err != nil {
+		http.Error(w, "failed to get setlist", http.StatusNotFound)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(setlist)
+}
+
+func (h *Handler) DeleteSetlist(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	setlistID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "invalid setlist id", http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.DeleteSetlist(r.Context(), setlistID)
+	if err != nil {
+		http.Error(w, "did not successfully delete setlist", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
